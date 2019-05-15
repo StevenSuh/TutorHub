e65b2784f798ef23f3ffa8b739e2bae30c1f4ee8
Steven Suh <stevenesuh@gmail.com>
2019-05-14 20:32:13 -0700

    add example of svg import

--- client/src/scripts/modules/LandingPage/Component.js
+++ client/src/scripts/modules/LandingPage/Component.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { ReactComponent as EditIcon } from 'src/assets/icons/edit_icon.svg';
 import style from './style.css?module';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div className={style.landing_page}>Landing Page</div>
+      <div className={style.landing_page}>
+        <h1>Landing Page</h1>
+        <EditIcon />
+      </div>
     );
   }
 }
